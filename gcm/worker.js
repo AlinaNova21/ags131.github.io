fd5c3f01bb461b19346b527c3cac644f413b6ce3
Adam Shumann <ashumann13@gmail.com>
2015-06-25 09:38:20 -0500

    Updates!

--- gcm/worker.js
+++ gcm/worker.js
@@ -1,16 +1,23 @@
 self.addEventListener('push', function(event) {  
-  console.log('Received a push message', event);
+	console.log('Received a push message', event);
 
-  var title = 'Yay a message.';  
-  var body = 'We have received a push message.';  
-  var icon = '/images/icon-192x192.png';  
-  var tag = 'simple-push-demo-notification-tag';
+	var title = 'Yay a message.';  
+	var body = 'We have received a push message.';  
+	var icon = '/images/icon-192x192.png';  
+	var tag = 'simple-push-demo-notification-tag';
 
-  event.waitUntil(  
-    self.registration.showNotification(title, {  
-      body: body,  
-      icon: icon,  
-      tag: tag  
-    })  
-  );  
+	event.waitUntil(  
+		self.registration.showNotification(title, {  
+			body: body + ' ' + localStorage.gcmtoken,  
+			icon: icon,  
+			tag: tag  
+		})  
+	);  
 });
+
+self.addEventListener('fetch',function(event){
+	var token = event.request.headers['x-token']
+	if(token)
+		localStorage.gcmtoken = token;
+	event.respondWith(fetch(event.request))
+})
